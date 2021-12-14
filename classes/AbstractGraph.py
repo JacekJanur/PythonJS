de9d67683c04d20ccbab6a34ce596bd22aa84e41
JacekJanur <jacekjanur1@gmail.com>
2021-12-14 18:31:27 +0100

    Sprawdzanie czy istnieje polaczenie  algorytmem BFS

--- classes/AbstractGraph.py
+++ classes/AbstractGraph.py
@@ -4,6 +4,7 @@ class AbstractGraph:
     def __init__(self, vertices, edges):
         self.__vertices = vertices
         self.__edges = edges
+        self.__graf = {x.getId():x.getName() for x in vertices}
     
     def getVertices(self):
         return self.__vertices
@@ -11,16 +12,41 @@ class AbstractGraph:
     def getEdges(self):
         return self.__edges
     
-    def getNeighbours(self, a):
+    def getGraf(self):
+        return self.__graf
+    
+    def getNeighbours(self, a): #wszyscy sąsiedzi danej krawędzi
         neighbours = []
         for x in self.getEdges():
             if x.getStart() == a:
                 neighbours.append(x.getEnd())
         return neighbours
         
-    def getFrom(self, a):
+    def getFrom(self, a): # skąd do danej krawędzi możemy dotrzeć
         neighbours = []
         for x in self.getEdges():
             if x.getEnd() == a:
                 neighbours.append(x.getStart())
         return neighbours
+    
+    def BFS(self, skad, do):    
+        for x in self.getVertices():
+            x.visited = False
+            
+        queue = []
+ 
+        queue.append(skad)
+        skad.visited = True
+ 
+        while queue:
+ 
+            skad = queue.pop(0)
+
+            for i in self.getNeighbours(skad):
+                if i.visited == False:
+                    queue.append(i)
+                    i.visited = True
+                    if i.getId() == do.getId():
+                        print("jest połączenie")
+                    
+            
--- main.py
+++ main.py
@@ -8,9 +8,24 @@ cityId = ITcityId()
 cities = []
 cities.append(City("Kraków", cityId.__next__()))
 cities.append(City("Nowy Sącz", cityId.__next__()))
+cities.append(City("Rytro", cityId.__next__()))
+cities.append(City("Piwniczna", cityId.__next__()))
+cities.append(City("Warszawa", cityId.__next__()))
+cities.append(City("Gdańsk", cityId.__next__()))
+cities.append(City("Wałbrzych", cityId.__next__()))
 
 connections = []
 connections.append(Connection(cities[0], cities[1]))
+connections.append(Connection(cities[1], cities[5]))
+connections.append(Connection(cities[1], cities[2]))
+connections.append(Connection(cities[2], cities[3]))
+connections.append(Connection(cities[1], cities[4]))
+connections.append(Connection(cities[5], cities[2]))
+connections.append(Connection(cities[5], cities[6]))
+
+connections.append(Connection(cities[4], cities[5]))
 
 graf = AbstractGraph(cities, connections)
-print(graf.getFrom(cities[0]))
+
+
+graf.BFS(cities[0], cities[6])
